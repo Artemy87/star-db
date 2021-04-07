13f7e955bfa52235e9a6cc0782cbfd5a6a92ad59
Artemy <artemij87work@gmail.com>
2021-04-07 19:30:21 +0300

    class client

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log('Hello World');
+// класс клиент для star wars API
+class SwapiServerce {
+  _apiBase = "https://swapi.dev/api";
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`
+      Could not fetch ${url},
+      received ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  }
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res;
+  }
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  }
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res;
+  }
+
+  getStarships(id) {
+    return this.getResource(`/starships/${id}`);
+  }
+}
+
+const swapi = new SwapiServerce();
+
+swapi.getPlanet(3).then((p) => {
+  console.log(p.name);
+});
